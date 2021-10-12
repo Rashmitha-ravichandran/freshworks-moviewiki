056092e80d5e376054131610e0d0b4ef428283b0
Rashmitha-ravichandran <rash1997mitha@gmail.com>
2021-10-12 15:41:09 +0530

    axios

--- .github/labeler/index.js
+++ .github/labeler/index.js
@@ -1,17 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const context = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event context: ${context}`);
-  const pr = JSON.stringify(github.context.head_ref, undefined, 2)
-  console.log(`The event pr: ${pr}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+axios.get('https://api.github.com/repos/Rashmitha-ravichandran/freshworks-moviewiki/branches/custom-_action_hello_world')
+    .then(response => {
+        console.log(response);
+        // console.log(response.data);
+        //res.send(response.data.status);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+// try {
+//   // `who-to-greet` input defined in action metadata file
+//   const nameToGreet = core.getInput('who-to-greet');
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = (new Date()).toTimeString();
+//   core.setOutput("time", time);
+//   // Get the JSON webhook payload for the event that triggered the workflow
+//   const context = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event context: ${context}`);
+//   const pr = JSON.stringify(github.context.head_ref, undefined, 2)
+//   console.log(`The event pr: ${pr}`);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
